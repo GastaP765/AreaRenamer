0dd406cc1a8a7480a20e9893b15cd929c84c381f
GastaP765 <gasta.psychic.bird@gmail.com>
2018-11-15 16:48:00 +0900

    Update AreaRenamer.py

--- AreaRenamer.py
+++ AreaRenamer.py
@@ -15,7 +15,7 @@ def rename():
 	ns2 = '_'
 
 	frn = mc.textField(name1, q=True, text=True)
-	ctn = mc.textField(name2, q=True, text=True)
+	cen = mc.textField(name2, q=True, text=True)
 	afn = mc.textField(name3, q=True, text=True)
 	aon = mc.radioCollection(rc1, q=True, select=True)
 
@@ -23,12 +23,16 @@ def rename():
 		ns1 = ''
 	if afn == '':
 		ns2 = ''
-	if ctn == '':
-		mc.warning('Base name is not specified')
 
 	if aon == 'alp':
-		for i in sl_name:
-			mc.rename(i, frn + ns1 + ctn + h + t + Acode[e] + ns2 + afn)
+		for i in range(cnt):
+			if cen == '':
+				ctn = sl_name[i]
+				mc.rename(sl_name[i], frn + ns1 + ctn + h + t + ns2 + afn)
+			else:
+				ctn = cen
+				mc.rename(sl_name[i], frn + ns1 + ctn + h + t + Acode[e] + ns2 + afn)
+
 			if e == 25:
 				e = 0
 				if t == '':
@@ -46,35 +50,51 @@ def rename():
 						t = Acode[f]
 			else:
 				e = e + 1
+			sl_name = mc.ls(sl=True)
 
 	elif aon == 'num':
+		if cen == '':
+			ctn = sl_name[i]
+		else:
+			ctn = cen
 		for i in range(cnt):
 			j = str(i+1)
 			mc.rename(sl_name[i], frn + ns1 + ctn + j + ns2 + afn)
+			sl_name = mc.ls(sl=True)
 
+	elif aon == 'none':
+		if cen == '':
+			ctn = sl_name[i]
+		else:
+			ctn = cen
+		mc.rename(sl_name[0], frn + ns1 + ctn + ns2 + afn)
 
-if mc.window('AreaRenamer', exists=True):
-	mc.deleteUI('AreaRenamer')
+def mainWin():
+	if mc.window('AreaRenamer', exists=True):
+		mc.deleteUI('AreaRenamer')
 
-win = mc.window('AreaRenamer', t='AreaRenamer', mxb=False, widthHeight=(300,150))
-mc.columnLayout(adj=True)
+	win = mc.window('AreaRenamer', t='AreaRenamer', mxb=False, widthHeight=(300,150))
+	mc.columnLayout(adj=True)
+	mc.window('AreaRenamer', e=True, widthHeight=(300,150))
 
-mc.frameLayout(l='Serial, Alphabet or Number')
-mc.rowLayout(nc=2, cat=[(1, 'left',50), (2, 'left', 50)])
-rc1 = mc.radioCollection()
-rb1 = mc.radioButton('alp', l='Alphabet', sl=True,)
-rb2 = mc.radioButton('num', l='Number')
-mc.setParent('..')
+	mc.frameLayout(l='Serial, Alphabet or Number')
+	mc.rowLayout(nc=3, cat=[(1, 'left',20), (2, 'left', 20), (3, 'left', 20)])
+	rc1 = mc.radioCollection()
+	rb1 = mc.radioButton('alp', l='Alphabet', sl=True,)
+	rb2 = mc.radioButton('num', l='Number')
+	rb3 = mc.radioButton('none', l='None')
+	mc.setParent('..')
 
-mc.frameLayout(l='setName')
-mc.rowLayout(nc=5, cw5=(95, 5, 95, 5, 95))
-name1 = mc.textField('name1', w=95, pht='BeforeName')
-mc.text(l='_')
-name2 = mc.textField('name2', w=95, pht='BaceName')
-mc.text(l='_')
-name3 = mc.textField('name3', w=95, pht='AfterName')
-mc.setParent('..')
+	mc.frameLayout(l='setName')
+	mc.rowLayout(nc=5, cw5=(95, 5, 95, 5, 95))
+	name1 = mc.textField('name1', w=95, pht='BeforeName')
+	mc.text(l='_')
+	name2 = mc.textField('name2', w=95, pht='BaceName')
+	mc.text(l='_')
+	name3 = mc.textField('name3', w=95, pht='AfterName')
+	mc.setParent('..')
 
-mc.button(w=200, h=30, l='rename', c='rename()')
+	mc.button(w=200, h=30, l='rename', c='rename()')
 
-mc.showWindow(win)
+	mc.showWindow(win)
+mainWin()
